b47d72ed342c05f2ab4639e3fdf3a166e6d7053d
mugelstad <mugelsta@wellesley.edu>
2018-12-18 10:37:48 -0500

    arranging folders

---
+++ MultiTaction_surface/run.bat
@@ -0,0 +1,1 @@
+@%~dp0\..\..\..\..\Cornerstone-SDK-2.1.2\examples\Javascript\common.bat %~dp0main.js %*
--- run.bat
+++
@@ -1,1 +0,0 @@
-@%~dp0\..\..\..\..\Cornerstone-SDK-2.1.2\examples\Javascript\common.bat %~dp0main.js %*
